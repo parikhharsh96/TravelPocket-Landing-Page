9be2fc774e51a07850d46c9c6420ee33c9676b33
harsh-parikh <parikhharsh1996@gmail.com>
2025-12-17 12:19:51 +0530

    header image change and extra gifts removed

--- src/components/main/packages-section.tsx
+++ src/components/main/packages-section.tsx
@@ -51,8 +51,8 @@ const packages = [
     icon: "bus",
     title: "Kailash Mansarovar Yatra 2026 | By Bus from Kathmandu",
     duration: "14 Nights / 15 Days",
-    image:  "/images/2.svg",
-    imageMob:"/images/2.svg",
+    image:  "/images/3.svg",
+    imageMob:"/images/by-road.svg",
     idealFor: [
       "Pickup: Kathmandu",
       "Duration: 14 Nights / 15 Days",
@@ -95,8 +95,8 @@ const packages = [
     icon: "plane",
     title: "Kailash Mansarovar Yatra 2026 | Lhasa Route via Flight",
     duration: "10 Nights / 11 Days",
-    image: "/images/3.svg",
-    imageMob:"/images/3.svg",
+    image: "/images/2.svg",
+    imageMob:"/images/2.svg",
      idealFor: [
       "Pickup: Kathmandu",
       "Duration: 10 Nights / 11 Days",
@@ -151,8 +151,8 @@ const packages = [
     icon: "mountain",
     title: "Kailash Mansarovar Door Darshan Yatra 2026 | via Limi-Lapcha Route",
     duration: "3 Nights / 8 Days",
-    image:  "/images/by-road.svg",
-    imageMob:"/images/by-road.svg",
+    image:  "/images/limi.svg",
+    imageMob:"/images/limi-mob.svg",
     idealFor: [
       "Pickup: Lucknow",
       "Duration: 7 Nights / 8 Days",
